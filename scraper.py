bf42d72ecbb339fd37b13f23422d92c45c4c56b1
d <jerryf6@uci.edu>
2026-04-25 16:20:56 -0700

    scraper config done

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,19 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    found_links = set()
+    if resp.status == 200 and resp.raw_response and resp.raw_response.content:
+        try:
+            soup = BeautifulSoup(resp.raw_response.content, "lxml")
+            for link in soup.find_all('a', href=True):
+                href = urljoin(resp.url, link['href'])
+                clean_url = href.split('#')[0]
+                found_links.add(clean_url)
+        except Exception as e:
+            print(e)
+
+    return list(found_links)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -23,7 +36,18 @@ def is_valid(url):
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
+        allowed_domains = [
+            ".ics.uci.edu",
+            ".cs.uci.edu",
+            ".informatics.uci.edu",
+            ".stat.uci.edu"]
         if parsed.scheme not in set(["http", "https"]):
+            return False
+        allowed = False
+        for domain in allowed_domains:
+            if parsed.netloc.endswith(domain):
+                allowed = True
+        if allowed == False:
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
